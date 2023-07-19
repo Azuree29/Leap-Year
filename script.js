504eb7a898f24442172a8e298b2b1926e33a2030
Azuree29 <123673411+Azuree29@users.noreply.github.com>
2023-07-19 15:44:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+
+let q1test = 15;
+
+if (q1test % 3 || q1test % 5 === 0) {
+    console.log("divisbile by 3 and 5");
+}  else {
+    console.log("not divisible by anything");
+}
+
+
+let year1 = 1999;
+let year2 = 2000;
+let year3 = 1900;
+let year4 = 2024;
+
+if (year1 % 400 == 0) {
+
+console.log ("is a leap year");
+} 
+
+else if ((year1 % 4 == 0)&& (year1 % 100 != 0)){
+console.log("is leap year");
+} 
+
+else {
+    console.log("is not leap year");
+}
+
+
+if (year2 % 400 == 0) {
+
+console.log ("is a leap year");
+} 
+
+else if ((year1 % 4 == 0)&& (year1 % 100 != 0)){
+console.log("is leap year");
+} 
+
+else {
+    console.log("is not leap year");
+}
+
+
+if (year3 % 400 == 0) {
+
+console.log ("is a leap year");
+} 
+
+else if ((year1 % 4 == 0)&& (year1 % 100 != 0)){
+console.log("is leap year");
+} 
+
+else {
+    console.log("is not leap year");
+}
+
+
+if (year4 % 400 == 0) {
+
+console.log ("is a leap year");
+} 
+
+else if ((year1 % 4 == 0)&& (year1 % 100 != 0)){
+console.log("is leap year");
+} 
+
+else {
+    console.log("is not leap year");
+}
